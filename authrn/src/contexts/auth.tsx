14b95f96dc29754ff84c305a0ac32fc740c0951c
Samuel Santos <samuellima280499@gmail.com>
2020-08-10 16:16:22 -0300

    Project Finished

--- authrn/src/contexts/auth.tsx
+++ authrn/src/contexts/auth.tsx
@@ -1,9 +1,12 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import * as auth from '../services/auth';
+import api from '../services/api';
 
 interface AuthContextData {
   signed: boolean;
   user: object | null;
+  loading: boolean;
   signIn(): Promise<void>;
   signOut(): void;
 }
@@ -12,19 +15,44 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<object | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadStoragedData() {
+      const storagedUser = await AsyncStorage.getItem('@RNAuth:user');
+      const storagedToken = await AsyncStorage.getItem('@RNAuth:token');
+
+      if (storagedUser && storagedToken) {
+        api.defaults.headers['Authorization'] = `Bearer ${storagedToken}`;
+        setUser(JSON.parse(storagedUser));
+        setLoading(false);
+      }
+    }
+
+    loadStoragedData();
+  }, []);
 
   async function signIn() {
     const response = await auth.signIn();
 
     setUser(response.user);
+
+    api.defaults.headers['Authorization'] = `Bearer ${response.token}`;
+
+    await AsyncStorage.setItem('@RNAuth:user', JSON.stringify(response.user));
+    await AsyncStorage.setItem('@RNAuth:token', response.token);
   }
 
   function signOut() {
-    setUser(null);
+    AsyncStorage.clear().then(() => {
+      setUser(null);
+    });
   }
 
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ signed: !!user, user, signIn, loading, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
